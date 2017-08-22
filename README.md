208eccdae450f6c01ec6f4d61936325cd9544fa9
Thomas Noe <thomas.noe@madwiremedia.com>
2017-08-21 19:16:06 -0600

    init commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# mass dig
+> dig the masses for fun and profit
+
+## what
+`mass-dig.sh` takes a list of possible domains and searches for [A records](https://support.dnsimple.com/articles/a-record/) quickly (by leveraging parallelism) using the `dig` command
+
+## why
+Presence of an A record can be used for TLD enumeration
+
+## how
+`./mass-dig.sh [TLD LIST] [THREADS (OPTIONAL)]`
+
+The TLD list should be newline separated, I.E
+```
+website.com
+website.co.uk
+website.io
+```
+
+### results
+The results are both
+1. Found hosts are appended to a `known_hosts.dug` file
+2. Each host's specific dig results are saved to a `[HOST].dug` file for further inspection
---
+++ mass-dig.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+PARALLEL=25
+
+function dig_host {
+  if [ -z "$1" ]; then
+    echo "[!] invalid args for dig_host"
+    exit 1
+  fi
+  host=$1
+
+  echo "[*] digging host $host"
+  res=$(dig "$host" ANY +noall +answer | grep -v "^;" | grep -v "^$")
+  if [ -z "$res" ]; then
+    echo "[*] result for host $host was empty"
+  else
+    echo "[*] result found for host $host"
+    echo "$res" > "${host}.dug"
+    echo "$host" >> "found_hosts.dug"
+  fi
+}
+
+export -f dig_host
+
+if [ -z "$1" ]; then
+  echo "[!] usage: $0 [FILE]";
+  exit 1;
+fi
+
+if [ -z "$2" ]; then
+  PARALLEL="$2"
+fi
+
+if [ ! -f "$1" ]; then
+  echo "[!] error: $1 is not a file"
+  exit 1
+fi
+
+file=$1
+
+echo "[*] scanning domains from $file en masse"
+cat $file | xargs -P $PARALLEL -I '{}' sh -c 'dig_host {}'
